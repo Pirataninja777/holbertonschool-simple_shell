5e270fd6473a5dc3aca73d81282bf3c3562c3f60
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-08-22 10:27:19 -0400

    updated exec file to include executable cases

--- exec.c
+++ exec.c
@@ -1,37 +1,54 @@
-#include "shell.h"
+#include "main.h"
+
 /**
- * execute_command - Executes a command in a child process.
- * @args: The array of arguments for the command to execute.
- *
- * Description: This function forks the current process to create a child
- * process. The child process attempts to execute the command specified
- * in `args[0]` using `execve`. If the execution fails, an error message
- * is printed. The parent process waits for the child process to complete.
+ * exec - executes the input received
+ * @args: arguments to execute
+ * Return: void
  */
-
-extern char **environ;
-
-void execute_command(char **args)
+void exec(char **args)
 {
-	pid_t pid;
-	int status;
+	char *line = NULL;
+	int status, status_exit;
+	pid_t child_pid;
+	char *command_path;
 
-	pid = fork();  /* Create a child process*/
-	if (pid == 0)  /* Child process*/
+	command_path = find_path(args[0]);
+	if (access(args[0], X_OK) != 0)
 	{
-		if (execve(args[0], args, environ) == -1)  /* Execute the command*/
+		if (command_path == NULL)
 		{
-			perror("Error");
+			fprintf(stderr, "%s: command not found\n", args[0]);
+			return;
 		}
-		exit(EXIT_FAILURE);
 	}
-	else if (pid < 0)  /* Fork failed*/
+
+	child_pid = fork();
+	if (child_pid == -1)
 	{
-		perror("Fork failed");
+		perror("fork");
 	}
-	else  /* Parent process*/
+	else if (child_pid == 0)
 	{
-		waitpid(pid, &status, 0);  /* Wait for the child to finish*/
+		if (execve(command_path, args, environ) == -1)
+		{
+			fprintf(stderr, "%s: command not found\n", args[0]);
+			free(line);
+			free(args[0]);
+			exit(EXIT_FAILURE);
+		}
+	}
+	else
+	{
+		wait(&status);
+	}
+
+	if (WIFEXITED(status))
+	{
+		status_exit = WEXITSTATUS(status);
+		if (status_exit != 0)
+		{
+			free(command_path);
+		}
 	}
 }
 
