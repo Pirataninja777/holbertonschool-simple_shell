84538a84f47bf13f575ca080edfc7530ee5941b6
abrah926 <abrahamvidalcastillo2@gmail.com>
2024-08-22 10:44:11 -0400

    added proper reference to header file

---
+++ getpath.c
@@ -0,0 +1,54 @@
+#include "shell.h"
+
+/**
+ * find_path - finds the full path of a command
+ * @command: command to find
+ *
+ * Return: full path of command, NULL if not found
+ */
+char *find_path(char *command)
+{
+	char *path = getenv("PATH");
+	char *path_copy, *token;
+	char *path_buffer;
+	struct stat st;
+
+	if (access(command, F_OK) == 0)
+	{
+		return (strdup(command));
+	}
+
+	if (path == NULL)
+	{
+		fprintf(stderr, "PATH variable not set\n");
+		return (NULL);
+	}
+
+	path_copy = strdup(path);
+	token = strtok(path_copy, ":");
+
+	while (token != NULL)
+	{
+		path_buffer = malloc(strlen(token) + strlen(command) + 2);
+		if (path_buffer == NULL)
+		{
+			free(path_copy);
+			return (NULL);
+		}
+
+		sprintf(path_buffer, "%s/%s", token, command);
+
+		if (stat(path_buffer, &st) == 0)
+		{
+			free(path_copy);
+			return (path_buffer);
+		}
+
+		free(path_buffer);
+		token = strtok(NULL, ":");
+	}
+
+	free(path_copy);
+	return (NULL);
+}
+
